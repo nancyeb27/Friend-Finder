30f2556da16d5902fce34e56d3e21cef363fcf24
Nancyb <nancyeb27@yahoo.com>
2018-12-16 18:25:27 -0700

    saving files

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [
+	
+	{
+		name: "John Doe",
+		photo: "#",
+		scores: [
+			
+		]
+	},
+	{
+		name: "Mike Smith",
+		photo: "#",
+		scores: [
+			
+		]
+	},
+	{
+		name: "Davey Jones",
+		photo: "#",
+		score: [
+			
+		]
+	}
+];
+
+module.exports = friends;
