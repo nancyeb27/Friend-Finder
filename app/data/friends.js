f67387679b0336899daf17ba7569beec648e1a2d
Nancyb <nancyeb27@yahoo.com>
2019-01-05 12:54:22 -0700

    working on img bug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,9 @@
 var friends = [
 	
-	{
-		name: "Tom Thumb",
-		photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn1.vectorstock.com%2Fi%2F1000x1000%2F79%2F30%2Fbusiness-man-cartoon-character-young-handsome-vector-14427930.jpg&imgrefurl=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fbusiness-man-cartoon-character-young-handsome-vector-14427930&docid=DOTUuTSy_AgR5M&tbnid=54nTPixht7auYM%3A&vet=10ahUKEwja-ubUvcXfAhUBNKwKHTrYDQQQMwhoKAAwAA..i&w=715&h=1080&hl=en&bih=723&biw=1536&q=MAN&ved=0ahUKEwja-ubUvcXfAhUBNKwKHTrYDQQQMwhoKAAwAA&iact=mrc&uact=8",
-		scores: [
-			"2",
-			"4",
-			"2",
-			"5",
-			"5",
-			"5",
-			"1",
-			"3",
-			"3",
-			"3"
-		]
-	},
 		{
 		name: "Davey Jones",
 		photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fe%2Feb%2FA_Cartoon_Man_Fixing_His_Cuffs.svg&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AA_Cartoon_Man_Fixing_His_Cuffs.svg&docid=GeFUfP4-jK0-DM&tbnid=8xHPLz0_oFOgUM%3A&vet=10ahUKEwja-ubUvcXfAhUBNKwKHTrYDQQQMwhuKAYwBg..i&w=816&h=1056&hl=en&bih=723&biw=1536&q=MAN&ved=0ahUKEwja-ubUvcXfAhUBNKwKHTrYDQQQMwhuKAYwBg&iact=mrc&uact=8",
-		score: [
-			"3",
-			"3",
-			"3",
-			"3",
-			"3",
-			"3",
-			"2",
-			"3",
-			"3",
-			"5"
-		]
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	}
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information from survey.
 // ===============================================================================
 var friends = require("../data/friends.js");
 
@@ -11,73 +9,53 @@ var friends = require("../data/friends.js");
 
 module.exports = function (app) {
 // API GET Requests
-  // Below code handles when users "visit" a page.
-  // ------------------------------
-    
+// ------------------------------
     app.get('/api/friends', function (req, res){
         res.json(friends);
-       
-
-
     });
 
     // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // When a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out the survey... this data is then sent to the server...
-  // Then the server saves the data to the friends.js array)
-  // ---------------------------------------------------------------------------
+   // ---------------------------------------------------------------------------
 
     app.post('/api/friends', function (req, res) {
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 10
-            
-        };
-            
-       
-       var userData = req.body;
-       var userScores = userData.scores;
-       console.log (userScores)
-       // To take the results of the user's name and photo, from the survey questions, to post and parse it
-       var userName = userData.name;
-       var userPhoto = userData.photo;
+        console.log(req.body.scores);
 
+    // Receive user details (name, photo, scores)          
+    var userData = req.body;
+   
+    // parseInt for scores
+    for(var i = 0; i < userData.scores.length; i++) {
+    userData.scores[i] = parseInt(userData.scores[i]);
+    console.log(userData.scores);
+      }
 
-
-       // The variable used to calculate the difference b/n the user's socres and the scores of each user
-       var totalDifference = 0;
-
-
+      // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    var bestFriendIndex = 0;
+    var minimumDifference = 40;
+   
 
        //loop through the friends data array of objects to get each friends scores
        for (var i = 0; i < friends.length - 1; i++) {
-           console.log(friends[i].name);
            totalDifference = 0;
 
-
-
-           //loop through that friends score and the users score and calculate the absolute difference between the two and push that to the total difference variable set above
-           for (var j = 0; j < 10; j++) {
-               // We calculate the difference between the scores and sum them into the totalDifference
-               totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-               // If the sum of differences is less then the differences of the current "best match"
-               if (totalDifference <= bestMatch.friendDifference) {
-                   console.log(totalDifference)
-
-
-
-                   // Reset the bestMatch to be the new friend.
-                   bestMatch.name = friends[i].name;
-                   bestMatch.photo = friends[i].photo;
-                   bestMatch.friendDifference = totalDifference;
-               }
-           }
+        //loop through that friends score and the users score and calculate the absolute difference between the two and push that to the total difference variable set above
+        for(var j = 0; j < friends[i].scores.length; j++) {
+         // We calculate the difference between the scores and sum them into the totalDifference
+         var difference = Math.abs(userData.scores[j] - friends[i].scores[j]);
+         totalDifference += difference;
+         console.log(totalDifference);
        }
 
+               // If the sum of differences is less then the differences of the current "best match"
+               if (totalDifference <= minimumDifference) {
+                  
+                // Reset the bestMatch to be the new friend.
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+              }
+            }
 
+           
 
        // The push method use to save user's data to the database
        friends.push(userData);
@@ -85,7 +63,7 @@ module.exports = function (app) {
 
 
        //The res.json method will return a JSON data with the user's match which was looped through frieds data array.
-       res.json(bestMatch);
+       res.json(friends[bestFriendIndex]);
    });
 };
 
