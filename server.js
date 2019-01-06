1ccfca4d2fe60980eebcda9bb8ed02116b73b145
Nancyb <nancyeb27@yahoo.com>
2019-01-05 17:24:53 -0700

    updating server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 var express = require("express");
 var path = require("path");
+var cors = require('cors')
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -11,6 +12,10 @@ var path = require("path");
 
 // Tells node that we are creating an "express" server
 var app = express();
+
+//allow OPTIONS on all resources
+app.options('*', cors())
+
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
